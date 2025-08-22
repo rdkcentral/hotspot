4d97df79bb85bd4655ccdfd3ac4bac68219be04e
Supriya Shree <supriyashree_ss@comcast.com>
2025-08-22 06:13:45 +0000

    RDKB-60325 : notify_tunnel_status failure in hotspot
    Reason for change: notify_tunnel_status fails in hotspot
    Test Procedure: TunnelStatus should get notified
    Risks: Low
    Priority: P2
    Signed-off-by:SupriyaShree_SS@comcast.com

--- source/hotspotfd/hotspotfd.c
+++ source/hotspotfd/hotspotfd.c
@@ -2049,7 +2049,7 @@ void hotspot_start()
 
 #ifdef WAN_FAILOVER_SUPPORTED
     rbusDataElement_t dataElements[1] = {
-        {"Device.X_COMCAST-COM_GRE.Tunnel.1.TunnelStatus", RBUS_ELEMENT_TYPE_EVENT, {NULL, NULL, NULL, CcspBaseIf_SendSignal_WithData_rbus, NULL, NULL}}
+        {"Device.X_COMCAST-COM_GRE.Tunnel.1.TunnelStatus", RBUS_ELEMENT_TYPE_EVENT, {NULL, NULL, NULL, NULL, NULL, NULL}}
     };
     ret = rbus_open(&handle, "HotspotTunnelEvent");
     if(ret != RBUS_ERROR_SUCCESS)
