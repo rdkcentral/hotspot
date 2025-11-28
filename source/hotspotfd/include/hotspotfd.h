5deb87ea1e1d12e3606c68f04dcbe01fbffacc1d
Foosball18 <sowmiya_chelliah@comcast.com>
2025-11-28 09:03:41 +0000

    RDKB-60325 : notify_tunnel_status failure in hotspot
    Reason for change: notify_tunnel_status fails in hotspot
    Test Procedure: TunnelStatus should get notified
    Risks: Low
    Priority: P2

--- source/hotspotfd/include/hotspotfd.h
+++ source/hotspotfd/include/hotspotfd.h
@@ -138,6 +138,8 @@ void hotspot_start();
 
 #ifdef UNIT_TEST_DOCKER_SUPPORT
 //bool set_tunnelstatus(char* status);
+rbusError_t TunnelStatus_GetStringHandler(rbusHandle_t handle, rbusProperty_t property, rbusGetHandlerOptions_t* opts);
+rbusError_t TunnelStatus_SetStringHandler(rbusHandle_t handle, rbusProperty_t property, rbusSetHandlerOptions_t* opts);
 void notify_tunnel_status(char *status);
 bool set_validatessid();
 bool get_validate_ssid();
